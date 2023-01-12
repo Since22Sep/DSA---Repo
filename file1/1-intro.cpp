8afb6062ec92bffeb21136a2650e330047cf4d7a
Dipanshu <dipanshukumar344@gmail.com>
2023-01-12 17:57:02 +0530

    basic info of linked list

---
+++ file1/1-intro.cpp
@@ -0,0 +1,22 @@
+ #include<iostream>
+using namespace std;
+
+int main(){
+   // What is Linked List:- It is a type of linear data structure which is formed by collection of nodes
+    // nodes :- it displays data and also the address of next node
+
+    // Why linked list:- Can you change the size of array at runtime..?? no
+    // In case of vector we are taking new space, copying value and it is taking some time... so linked list is preferred
+
+    // Linked List:- 
+    // advantages..  -> It has dynamic data structure ( we can grow or shrink at runtime therefore no memory wastage) 
+    //  ->insertion and deletion is easy in this case as no shift is needed. 
+ /*
+Drawbacks of Linked Lists: 
+  - Random access is not allowed. We have to access elements sequentially starting from the first node(head node). So we cannot do a binary search with linked lists efficiently with its default implementation. 
+  - Extra memory space for a pointer is required with each element of the list. 
+  - Searching for an element is costly and requires O(n) time complexity.
+ */
+    // Types of linked list:- i) Singly LL ii)Doubly LL iii) Circular LL iv)Circular doubly LL
+    return 0;
+}

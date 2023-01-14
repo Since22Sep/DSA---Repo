04cb357ab3acdc0004c95ed74aed4d8251a6d8a0
Dipanshu <dipanshukumar344@gmail.com>
2023-01-14 22:57:00 +0530

    Middle of LL using second approcah

--- file2/2-middleOfLL.cpp
+++ file2/2-middleOfLL.cpp
@@ -35,6 +35,27 @@ Node* findMiddle(Node* head){
     }return temp;
 }
 
+// Optimized Solution 
+Node* getMiddle(Node* head){
+    if(head == NULL || head->next == NULL){
+        return NULL;
+    }
+    // 2 nodes
+    if(head->next->next == NULL){
+        return head->next;
+    }
+    Node* slow = head;
+    Node* fast = head->next;
+
+    while(fast != NULL){
+        fast = fast->next;
+        if(fast != NULL){
+            fast = fast->next;
+        }
+        slow = slow->next;
+    }return slow;
+}
+
 int main(){
     
     return 0;

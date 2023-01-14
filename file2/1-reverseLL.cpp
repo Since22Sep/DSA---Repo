ab3b07f0737f6a920b1584b7cc5cdc528ce1cbd4
Dipanshu <dipanshukumar344@gmail.com>
2023-01-14 22:15:58 +0530

    Reversing a Linked List(Iterative Solution)

---
+++ file2/1-reverseLL.cpp
@@ -0,0 +1,35 @@
+ #include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int d){
+        this->data = data;
+        this->next = next;
+    }
+};
+
+Node* reverseLlinkedList(Node* head){
+    if(head == NULL || head->next == NULL){
+        return head;
+    }
+
+    Node* prev = NULL;
+    Node* curr = head;
+    Node* forward = NULL;
+
+    while(curr != NULL){
+        forward = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = forward;
+    }return prev;
+}
+
+int main(){
+    // Reverse a Linked List
+    return 0;
+}

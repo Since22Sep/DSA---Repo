079f6c3bc332d7b4ebe7125abddf39359d5730df
Dipanshu <dipanshukumar344@gmail.com>
2023-01-15 04:51:04 +0530

    data replacement is not allowed

--- file6/1-sort0s1s2s.cpp
+++ file6/1-sort0s1s2s.cpp
@@ -51,6 +51,64 @@ Node* sortList(Node* head){
     return head;
 }
 
+//  Approach 2:-
+
+void insertAtTail(Node* &tail, Node* &curr){
+    tail->next = curr;
+    tail = curr;
+}
+Node* sortingList(Node* head){
+    // creating 3 dummy list
+    Node * zeroHead = new Node(-1);
+    Node* zeroTail = zeroHead;
+
+    Node* oneHead = new Node(-1);
+    Node* oneTail = oneHead;
+
+    Node* twoHead = new Node(-1);
+    Node* twoTail = twoHead;
+
+    Node* curr = head;
+
+//    create separate list for 0s 1s and 2s
+   while(curr != NULL){
+    int value = curr->data;
+    if(value == 0){
+        insertAtTail(zeroTail , curr);
+    }
+    else if(value == 1){
+        insertAtTail(oneTail , curr);
+    }
+    else if(value == 2){
+        insertAtTail(twoTail , curr);
+    }
+    curr =curr->next;
+   }
+
+//    merge all three list
+    if(oneHead->next != NULL){
+        // 1s list is not empty
+        zeroTail->next = oneHead->next;
+    }
+    else{
+        // 1s list is empty
+        zeroTail->next = twoHead->next;
+    }
+    oneTail->next = twoHead->next;
+    twoTail-> next = NULL;
+
+    // head setup
+    head = zeroHead->next;
+
+    // delete all 3 dummy list
+    delete zeroHead;
+    delete oneHead;
+    delete twoHead;
+
+    return head;
+
+}
+
 int main(){
     // Sort 0s 1s and 2s in linked list
     /*

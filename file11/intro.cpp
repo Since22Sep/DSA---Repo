230171c572ac8aeca3dc96b18e69c96bcfe1850f
Dipanshu <dipanshukumar344@gmail.com>
2023-01-17 02:16:06 +0530

    stack Implementation using LInked List

--- file11/intro.cpp
+++ file11/intro.cpp
@@ -53,6 +53,84 @@ class Stack{
     }
 };
 
+// Stack implementation using Linked List
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+        this->data = data;
+        this->next = next;
+    }
+};
+
+class Stack1{
+    public:
+    int size;
+    Node* head;
+    
+    Stack1(int size){
+        this->size = size;
+        this->head = NULL;
+    }
+
+    void push(int data){
+        int cnt = 0;
+        Node* temp = head;
+        while(temp != NULL){
+            temp =temp->next;
+            cnt++;
+        }
+        if(cnt < size){
+            // insert at Head
+            Node* newNode = new Node(data);
+            if(head == NULL){
+                head = newNode;
+                return;
+            }
+            else{
+                newNode->next = head;
+                head = newNode;
+            }
+        }
+        else{
+            cout<<"Stack Overflow"<<endl;
+        }
+    }
+
+    void peek(){
+        if(head == NULL){
+            cout<<"Stack is empty"<<endl;
+        }
+        else{
+            cout<<head->data<<endl;
+        }
+    }
+
+    void pop(){
+        if(head == NULL){
+            cout<<"Stack is Underflow"<<endl;
+        }
+        else{
+            Node* temp = head;
+            cout<<"Popped element is "<<temp->data<<endl;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    bool isEmpty(){
+        if(head == NULL){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+};
+
 int main(){
     // creation of stack
     stack<int> s;

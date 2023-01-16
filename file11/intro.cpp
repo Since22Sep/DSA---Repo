ea921e3d91104b7393b0fc12e5058fe8d8c3dce0
Dipanshu <dipanshukumar344@gmail.com>
2023-01-17 01:59:40 +0530

    Stack implementation using Arrays

--- file11/intro.cpp
+++ file11/intro.cpp
@@ -2,6 +2,57 @@
  #include<stack>
 using namespace std;
 
+class Stack{
+    // properties
+    public:
+    int *arr;
+    int top;
+    int size;
+
+    // behaviour
+    Stack(int size){
+        this->size = size;
+        arr = new int [size];
+        top = -1;
+    }
+
+    int push(int element){
+        if(size - top > 1){
+            top++;
+            arr[top] = element;
+        }
+        else{
+            cout<<"Stack Overflow"<<endl;
+        }
+    }
+
+    int pop(){
+        if(top >= 0){
+            top --;
+        }
+        else{
+            cout<<"Stack Underflow"<<endl;
+        }
+    }
+
+    int peek(){
+        if(top >= 0 && top < size){
+            return arr[top];
+        }
+        else{
+            cout<<"Stack is empty"<<endl;
+            return -1;
+        }
+    }
+
+    bool isEmpty(){
+        if(top == -1){
+            return true;
+        }
+        return false;
+    }
+};
+
 int main(){
     // creation of stack
     stack<int> s;
@@ -23,5 +74,7 @@ int main(){
     }
     
     cout<<"size of stack is "<<s.size()<<endl;
+
+
     return 0;
 }
